7059c6bcde24130d91765d317453f95e24bd3bea
pradhan <techbulletinprime@gmail.com>
2023-08-20 10:56:36 +0530

    Inital commit

---
+++ src/main/java/com/monitoring/app/SpringBootPrometheusApplication.java
@@ -0,0 +1,22 @@
+package com.monitoring.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootPrometheusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrometheusApplication.class, args);
+	}
+	
+	@GetMapping("/testapp")
+	public String test() {
+		return "Prometheus Test";
+	}
+	
+
+}
